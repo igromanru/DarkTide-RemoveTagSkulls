35057514d8711a09b83dc11e5a37793dc3bdff50
igromanru <igromanru@yahoo.de>
2024-11-14 20:43:07 +0100

    feat: Rework the approach to keep other infomation about the tagget enemy on the screen

--- scripts/RemoveTagSkulls.lua
+++ scripts/RemoveTagSkulls.lua
@@ -2,21 +2,19 @@ local mod = get_mod("RemoveTagSkulls")
 
 local SettingNames = mod:io_dofile("RemoveTagSkulls/scripts/setting_names")
 
--- local ThreatSkullTemplate = "scripts/ui/hud/elements/world_markers/templates/world_marker_template_unit_threat"
--- local VeteranThreatSkullTemplate = "scripts/ui/hud/elements/world_markers/templates/world_marker_template_unit_threat_veteran"
-
 local marker_type_unit_threat = "unit_threat"
 local marker_type_unit_threat_veteran = "unit_threat_veteran"
 
-mod:hook(CLASS.HudElementWorldMarkers, "event_add_world_marker_unit", function(func, self, marker_type, unit, callback, data)
-	mod:debug("--- [HudElementWorldMarkers:event_add_world_marker_unit] ---")
-    if mod:get(SettingNames.RemoveTagSkull) and marker_type == marker_type_unit_threat then
-        return
-    end
-    if mod:get(SettingNames.RemoveVeteranTagSkull) and marker_type == marker_type_unit_threat_veteran then
-        return
+mod:hook(CLASS.HudElementWorldMarkers, "_template_by_type", function(func, self, marker_type, clone)
+	mod:debug("--- [HudElementWorldMarkers:_template_by_type] ---")
+    mod:debug("marker_type: %s", marker_type)
+    if (mod:get(SettingNames.RemoveTagSkull) and marker_type == marker_type_unit_threat)
+        or (mod:get(SettingNames.RemoveVeteranTagSkull) and marker_type == marker_type_unit_threat_veteran) then
+        local template = table.clone(self._marker_templates[marker_type])
+        template.max_distance = 0
+        return template
     end
     mod:debug("--------------------")
 
-	func(self, marker_type, unit, callback, data)
+	return func(self, marker_type, clone)
 end)
